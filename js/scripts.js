c4cdf93e1e419f6e276aecffc6cbc3ce18df7509
Matt Nardoni and Susan Southall <matt@>
2018-03-27 14:46:36 -0700

    Add button to show and hide answer.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,22 @@
 $(document).ready(function() {
-  $("button").click(function(event){
+  $("#question").click(function(event){
   event.preventDefault();
-  var vowels = 'you can do it'.split("");
-  // vowels.split("");
-  alert(vowels);
-  // debugger;
+  $("#help").click(function(){
+    $("p#answer").toggle();
+    $("#help").html("Click to hide answer.");
+    
+  })
+  var string = 'You can do it!';
+  var vowels = 'You can do it!'.split("");
+
   for (var i = 0; i<vowels.length;i++) {
     if (vowels[i] === "a" || vowels[i]==="e" || vowels[i]==="i" || vowels[i]==="o" || vowels[i]==="u") {
        vowels.splice(i,1,"-");
        var word = vowels.join("");
+       var newWord = word;
     };
   };
-    console.log(word);
-
-  //   alert(vowels);
+  $("p#puzzle").text(word);
+  $("p#answer").text(string);
   });
 });
-// debugger;
-// for (i=0;i<vowels.length; i++){
-//   if (i === "a" || "e" || "i" || "o" || "u") {
-//     vowels.splice(if (true) {
-//
-//     });
-// }alert(vowels);
-// };
